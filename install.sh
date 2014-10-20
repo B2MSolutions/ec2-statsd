e555021a24accfb473933c4bb602d9485c0d5b68
roylines <roy@roylines.co.uk>
2014-10-20 17:15:41 +0100

    added echo logging

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -v
 set -e
+echo 'update, upgrade and install'
 sudo apt-get update
 sudo apt-get upgrade --assume-yes
 sudo apt-get install --assume-yes apache2 apache2-mpm-worker apache2-utils apache2.2-bin apache2.2-common libapr1 libaprutil1 libaprutil1-dbd-sqlite3 build-essential python3.2 python-dev libpython3.2 python3-minimal libapache2-mod-wsgi libaprutil1-ldap memcached python-cairo-dev python-django python-ldap python-memcache python-pysqlite2 sqlite3 erlang-os-mon erlang-snmp rabbitmq-server bzr expect libapache2-mod-python python-setuptools
 sudo easy_install django-tagging zope.interface twisted txamqp
+
+echo 'wget and extract graphite, carbon and whisper'
 cd ~
 wget https://launchpad.net/graphite/0.9/0.9.10/+download/graphite-web-0.9.10.tar.gz
 wget https://launchpad.net/graphite/0.9/0.9.10/+download/carbon-0.9.10.tar.gz
@@ -11,13 +14,21 @@ wget https://launchpad.net/graphite/0.9/0.9.10/+download/whisper-0.9.10.tar.gz
 tar -zxvf graphite-web-0.9.10.tar.gz
 tar -zxvf carbon-0.9.10.tar.gz
 tar -zxvf whisper-0.9.10.tar.gz
+
+echo 'install whisper'
 cd whisper*
 sudo python setup.py install
+
+echo 'install carbon'
 cd ../carbon*
 sudo python setup.py install
+
+echo 'install graphite'
 cd ../graphite*
 sudo python check-dependencies.py
 sudo python setup.py install
+
+echo 'configure graphite'
 cd /opt/graphite/conf
 sudo cp carbon.conf.example carbon.conf
 echo '[stats]' > storage-schemas.conf
