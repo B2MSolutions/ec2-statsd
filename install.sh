37c7461ac30b2fed934c19bfcc1d82cf7e45acc3
michaelrowe <michael.rowe@b2m-solutions.com>
2016-10-07 15:58:45 +0100

    Changed install to access git using https

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ sudo apt-add-repository ppa:chris-lea/node.js -y
 sudo apt-get update --assume-yes
 sudo apt-get install nodejs git --assume-yes
 cd /opt
-sudo git clone git://github.com/etsy/statsd.git
+sudo git clone https://github.com/etsy/statsd.git
 
 echo '{ graphitePort: 2003, graphiteHost: "127.0.0.1", port: 8125 }' | sudo tee statsd/localConfig.js
 
